c0d02cb6f0d3d3847171990df7be1e451709d267
Romanos Trechlis <r.trechlis@gmail.com>
2017-09-08 15:27:32 +0300

    fix bug

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -4,14 +4,31 @@ import (
 	"flag"
 	"net/http"
 	"fmt"
+	"os"
+	"syscall"
+	"os/signal"
+	"log"
 )
 
 func main() {
 	path := flag.String("path", "./", "path of directory to serve")
 	port := flag.Int("port", 8080, "port for file server")
-	fs := http.FileServer(http.Dir(*path))
-	http.Handle("/", fs)
+	flag.Parse()
 
-	fmt.Printf("Listening to localhost:%d", *port)
-	http.ListenAndServe(fmt.Sprintf("localhost:%d", *port), nil)
+	stop:= make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		fs := http.FileServer(http.Dir(*path))
+		http.Handle("/", fs)
+
+		log.Printf("Listening to localhost:%d...\n", *port)
+		err := http.ListenAndServe(fmt.Sprintf("localhost:%d", *port), nil)
+		if err != nil {
+
+		}
+	}()
+
+	<-stop
+	log.Printf("Server was stopped gracefully.")
 }
